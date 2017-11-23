7dd57a3f5e8a9f4dca1c26e386eefa99dc2f5afe
Alexis <alexis@Alexiss-MacBook-Pro.local>
2017-11-23 18:09:26 +0000

    Test not yet passing, some progress in refactoring

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var childProcess = require('child_process');
+var commands = ["init", "add"];
 
 console.log("You launched githead!");
 
-if (getGitHeadArgs().length) {
-  eval(getGitHeadArgs()[0])();
-} else {
-  console.log("You didn't tell githead to do anything!");
+var wrapper = {
+  getGitHeadArgs: function () {
+    return process.argv.slice(2, process.argv.length);
+    }
 }
 
-function getGitHeadArgs () {
-  return process.argv.slice(2, process.argv.length);
+function runEverything() {
+  if (wrapper.getGitHeadArgs().length) {
+    if (!commands.includes(wrapper.getGitHeadArgs()[0])) {
+      console.log("This is not a command you can run with githead");
+    }
+    eval(wrapper.getGitHeadArgs()[0])();
+    // console.log(eval(getGitHeadArgs()[0])());
+  } else {
+    console.log("You didn't tell githead to do anything!");
+  }
 }
+
+// function getGitHeadArgs () {
+//   return process.argv.slice(2, process.argv.length);
+// }
 
 function init () {
   console.log("Helpful message about how git works");
@@ -23,15 +36,24 @@ function add() {
   console.log("This explains how git add works");
   var gitResponse = executeGitCommand();
   console.log(gitResponse);
+  return gitResponse;
 }
 
 function executeGitCommand () {
-  var gitCommand = childProcess.spawnSync('git', getGitHeadArgs());
+  var gitCommand = childProcess.spawnSync('git', wrapper.getGitHeadArgs());
   var gitResponse = gitCommand.stdout.toString() || gitCommand.stderr.toString();
   return gitResponse;
 }
 
+function unknownCommand() {
+  console.log("This is not a command you can run with githead");
+}
+
 module.exports = {
   init : init,
-  add : add
+  add : add,
+  runEverything : runEverything,
+  wrapper: wrapper
 };
+
+runEverything();
--- test/testIndex.js
+++ test/testIndex.js
@@ -3,36 +3,71 @@ var index = require('../index.js');
 var proxyquire = require('proxyquire');
 var sinon = require('sinon');
 
-describe('.init', function () {
-  it('returns a helpful message about "git init" to the command line', function () {
-    var consoleSpy = sinon.spy(console, "log");
-    index.init();
-    assert(consoleSpy.calledWith("Helpful message about how git works"));
-    console.log.restore();
-  });
-  it('executes "git init"', function () {
+// describe('.init', function () {
+//   it('returns a helpful message about "git init" to the command line', function () {
+//     var consoleSpy = sinon.spy(console, "log");
+//     index.init();
+//     assert(consoleSpy.calledWith("Helpful message about how git works"));
+//     console.log.restore();
+//   });
+//   it('executes "git init"', function () {
+//
+//     var index = proxyquire('../index.js', {
+//       'child_process': {
+//         spawnSync: function (command, args) {
+//                       return {
+//                         stdout: {
+//                           toString: () => "git init success string"
+//                         }
+//                       };
+//                     }
+//       }
+//     });
+//
+//     sinon.assert.match(index.init(), "git init success string");
+//   })
+//
+//   describe('.add', function() {
+//     it('returns a helpful message about "git add" to the command line', function() {
+//       var consoleSpy = sinon.spy(console, "log");
+//       index.add();
+//       assert(consoleSpy.calledWith("This explains how git add works"));
+//       console.log.restore();
+//     });
+//     it('executes "git add"', function() {
+//       var index = proxyquire('../index.js', {
+//         'child_process': {
+//           spawnSync: function(command, args) {
+//             return {
+//               stdout: {
+//                 toString: () => "git add success string"
+//               }
+//             }
+//           }
+//         }
+//       });
+//       sinon.assert.match(index.add(), "git add success string");
+//     });
+//
+//   });
 
-    var index = proxyquire('../index.js', {
-      'child_process': {
-        spawnSync: function (command, args) {
-                      return {
-                        stdout: {
-                          toString: () => "git init success string"
-                        }
-                      };
-                    }
-      }
-    });
-
-    sinon.assert.match(index.init(), "git init success string");
-  })
-
-  describe('.add', function() {
-    it('returns a helpful message about "git add" to the command line', function() {
+  describe('unknown command', function() {
+    // it('returns message', function () {
+    //   var consoleSpy = sinon.spy(console, "log");
+    //   index.unknownCommand();
+    //   assert(consoleSpy.calledWith("This is not a command you can run with githead"));
+    //   console.log.restore();
+    //   consoleSpy.reset();
+    // });
+    it('returns a message notifying you that the command does not exist', function() {
       var consoleSpy = sinon.spy(console, "log");
-      index.add();
-      assert(consoleSpy.calledWith("This explains how git add works"));
+      var arr = ['lol'];
+      var stub = sinon.stub(index.wrapper, "getGitHeadArgs").returns(arr);
+      // need the returns part of the method above to be correct
+      // intermediary test is to check that unknown method is called
+      index.runEverything();
+      assert(consoleSpy.calledWith("This is not a command you can run with githead"));
       console.log.restore();
     });
   });
-});
+// });
