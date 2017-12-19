1f1f1451b4cde3316f8b15fae8c6fcdff08c4d32
Jon Sanders <jonsanders101@aol.com>
2017-12-19 12:41:45 +0000

    Refactor Index tests with before hook

--- test/testIndex.js
+++ test/testIndex.js
@@ -3,12 +3,28 @@ var index = require('../index.js');
 var proxyquire = require('proxyquire');
 var sinon = require('sinon');
 
-describe('#welcome', () => {
+describe('Index', () => {
+  var printerMock, printerSpy, index;
+  before( () => {
+    printerSpy = sinon.spy();
+    printerMock = {
+      print: printerSpy
+    };
+    index = proxyquire('../index.js', {
+      'child_process': {
+        spawnSync: function(command, args) {
+          return {
+            stdout: {
+              toString: () => "git init success string"
+            }
+          };
+        }
+      }
+    });
+  });
+  describe('#welcome', () => {
+
     it('prints a welcome message', () => {
-      var printerSpy = sinon.spy();
-      var printerMock = {
-        print: printerSpy
-      };
       index.welcome(printerMock);
       assert(printerSpy.calledWith("You launched githead!"));
     });
@@ -16,69 +32,21 @@ describe('#welcome', () => {
 
   describe('#init', () => {
     it('prints a helpful message about "git init"', () => {
-      var printerSpy = sinon.spy();
-      var printerMock = {
-        print: printerSpy
-      };
       index.init(printerMock);
       assert(printerSpy.calledWith("Your directory is now initialised with git. That means everything in this directory and any sub--directories is being tracked.\nHints:\n> Add a '.gitignore' file to stop certain files from being tracked\n> Add a remote repository on GitHub to share the work in this directory with others."));
     });
     it('executes "git init"', function() {
-      var printerSpy = sinon.spy();
-      var printerMock = {
-        print: printerSpy
-      };
-       var index = proxyquire('../index.js', {
-         'child_process': {
-           spawnSync: function(command, args) {
-             return {
-               stdout: {
-                 toString: () => "git init success string"
-               }
-             };
-           }
-         }
-       });
-       assert.equal(index.init(printerMock), "git init success string");
-     });
-     it('prints "git init" success string', () => {
-       var printerSpy = sinon.spy();
-       var printerMock = {
-         print: printerSpy
-       };
-        var index = proxyquire('../index.js', {
-          'child_process': {
-            spawnSync: function(command, args) {
-              return {
-                stdout: {
-                  toString: () => "git init success string"
-                }
-              };
-            }
-          }
-        });
-        index.init(printerMock)
-        assert(printerSpy.calledWith("git init success string"));
-     });
+      assert.equal(index.init(printerMock), "git init success string");
+    });
+    it('prints "git init" success string', () => {
+      index.init(printerMock)
+      assert(printerSpy.calledWith("git init success string"));
+    });
   });
 
-// describe('.init', function() {
-//   it('executes "git init"', function() {
-//
-//     var index = proxyquire('../index.js', {
-//       'child_process': {
-//         spawnSync: function(command, args) {
-//           return {
-//             stdout: {
-//               toString: () => "git init success string"
-//             }
-//           };
-//         }
-//       }
-//     });
-//
-//     sinon.assert.match(index.init(), "git init success string");
-//   })
+})
+
+
 //   describe('.helpme', function() {
 //     it('returns some helpful information', function() {
 //       var consoleSpy = sinon.spy(console, "log");
