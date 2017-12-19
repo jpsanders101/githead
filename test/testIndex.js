2efa7d20e1e030305f59875d574dff216905365b
Jon Sanders <jonsanders101@aol.com>
2017-12-19 12:28:47 +0000

    Refactor test for executing git init

--- test/testIndex.js
+++ test/testIndex.js
@@ -23,16 +23,27 @@ describe('#welcome', () => {
       index.init(printerMock);
       assert(printerSpy.calledWith("Your directory is now initialised with git. That means everything in this directory and any sub--directories is being tracked.\nHints:\n> Add a '.gitignore' file to stop certain files from being tracked\n> Add a remote repository on GitHub to share the work in this directory with others."));
     });
+    it('executes "git init"', function() {
+      var printerSpy = sinon.spy();
+      var printerMock = {
+        print: printerSpy
+      };
+       var index = proxyquire('../index.js', {
+         'child_process': {
+           spawnSync: function(command, args) {
+             return {
+               stdout: {
+                 toString: () => "git init success string"
+               }
+             };
+           }
+         }
+       });
+       assert.equal(index.init(printerMock), "git init success string");
+     });
   });
 
 // describe('.init', function() {
-//   it('returns a helpful message about "git init" to the command line', function() {
-//     var consoleSpy = sinon.spy(console, "log");
-//     index.init();
-//     assert(consoleSpy.calledWith("Helpful message about how git works"));
-//     console.log.restore();
-//   });
-//
 //   it('executes "git init"', function() {
 //
 //     var index = proxyquire('../index.js', {
